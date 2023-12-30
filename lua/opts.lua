13badf20409d71439c68ab37554979087e3bcc54
paivabenja <paivabenja05@gmail.com>
2023-12-30 08:08:44 -0300

    macros recording shows in lualine and bit more idk

--- lua/opts.lua
+++ lua/opts.lua
@@ -8,7 +8,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.opt.smartcase = false  -- Don't ignore case with capitals
+vim.opt.smartcase = false -- Don't ignore case with capitals
 vim.opt.smartindent = true -- Insert indents automatically
 
 vim.opt.number = true
@@ -21,3 +21,5 @@ vim.opt.sidescrolloff = 8
 vim.opt.colorcolumn = "100"
 
 vim.opt.spelllang = { "en", "es" }
+
+vim.opt.breakindent = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,26 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<C-g>", require("telescope.builtin").live_grep)
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files)
+			vim.keymap.set("n", "<C-g>", require("telescope.builtin").live_grep)
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      local builtin = require("telescope.builtin")
-
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
